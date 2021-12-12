3a48cd360a2be56090d4c7224386036f0d782029
jyy1554 <jyy1556@gmail.com>
2021-12-12 21:40:52 +0900

    일정 항목 추가 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map(todo => (
+        //map을 사용하여 컴포넌트로 변활할 때는 key props를 전달해주어야함!
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
