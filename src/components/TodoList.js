70d329cf8f978cfd942742a2afbf6df73f888f90
jyy1554 <jyy1556@gmail.com>
2021-12-12 22:57:55 +0900

    삭제 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map(todo => (
         //map을 사용하여 컴포넌트로 변활할 때는 key props를 전달해주어야함!
-        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
       ))}
     </div>
   );
