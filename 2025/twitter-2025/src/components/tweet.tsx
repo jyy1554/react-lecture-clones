975e7bb589163c48ff232153a8cd1f5c1b068d9f
jyy1554 <jyy1556@gmail.com>
2025-03-31 21:26:19 +0900

    feat: edit tweet(Code challenge)

--- 2025/twitter-2025/src/components/tweet.tsx
+++ 2025/twitter-2025/src/components/tweet.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db } from "../firebase";
 import { deleteDoc, doc } from "firebase/firestore";
+import { useState } from "react";
+import EditTweetForm from "./edit-tweet-form";
 
 const Wrapper = styled.div`
   display: grid;
@@ -41,8 +43,22 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
+const EditButton = styled.button`
+  background-color: #1d9bf0;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
 export default function Tweet({username, tweet, userId, id}: ITweet) {
   const user = auth.currentUser;
+  const [edit, setEdit] = useState(false);
   const onDelete = async() => {
     const ok = confirm("Are you sure you want to delete this tweet?");
     if(!ok || user?.uid !== userId) return;
@@ -51,16 +67,21 @@ export default function Tweet({username, tweet, userId, id}: ITweet) {
     } catch(e) {  
       console.log(e);
     } finally {
-
     }
   };
+  const onEdit = () => setEdit((prev) => !prev);
 
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
-        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
+        {!edit ? <Payload>{tweet}</Payload> : <EditTweetForm tweet={tweet} userId={userId} id={id} setEdit={setEdit} />}
+        {user?.uid === userId && !edit ? 
+          <>
+            <EditButton onClick={onEdit}>Edit</EditButton>
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          </> : null
+        }
       </Column>
       <Column>
         <Photo src="/github-logo.svg" />
